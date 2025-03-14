01e3c5a86b32b0ab21bb4d9e6306224116e93572
Lizeweski <128433107+LIZEWESKI@users.noreply.github.com>
2025-03-14 22:02:15 +0000

    fixed link update routing

--- resources/js/Components/ui/progress.jsx
+++ resources/js/Components/ui/progress.jsx
@@ -9,7 +9,7 @@ function Progress({
   ...props
 }) {
   return (
-    <ProgressPrimitive.Root
+    (<ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
         "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
@@ -20,7 +20,7 @@ function Progress({
         data-slot="progress-indicator"
         className="bg-primary h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
-    </ProgressPrimitive.Root>
+    </ProgressPrimitive.Root>)
   );
 }
 
