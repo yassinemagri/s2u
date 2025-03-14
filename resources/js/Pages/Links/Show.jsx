80643868bec4eaac1391e23eb2796825c11643f9
Yassine Magri <77500416+yassinemagri@users.noreply.github.com>
2025-03-14 03:22:28 +0000

    debugging show

--- resources/js/Pages/Links/Show.jsx
+++ resources/js/Pages/Links/Show.jsx
@@ -1,33 +1,44 @@
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
-import { ExternalLink, Youtube, Moon, Sun } from "lucide-react";
+import { CircleHelp, Clock8, ExternalLink, Youtube } from "lucide-react";
 import Layout from "@/Components/layout/Layout";
 
-const Show = (props)=> {
-  console.log(props)
+const Show = ({link})=> {
   const [isSubscribed, setIsSubscribed] = useState(false);
   const [countdown, setCountdown] = useState(3);
-  const [apiData, setapiData] = useState('links');
+  const [apiData, setapiData] = useState(link);
+  const {id,title,unlock_link,channel_link,description,updated_at} = apiData
+
 
   useEffect(() => {
+    const handleBlur = () => {
+      clearTimeout(timer); 
+    };
+  
+    window.addEventListener('blur', handleBlur);
+  
     let timer;
     if (isSubscribed && countdown > 0) {
       timer = setTimeout(() => {
         setCountdown(countdown - 1);
       }, 1000);
     }
-    return () => clearTimeout(timer);
+  
+    return () => {
+      clearTimeout(timer); 
+      window.removeEventListener('blur', handleBlur);
+    };
   }, [isSubscribed, countdown]);
 
   const getSubscribeLink = () => {
-    if (!apiData?.channelUrl.includes("?sub_confirmation=1")) {
-      return `${apiData?.channelUrl}?sub_confirmation=1`;
+    if (!channel_link.includes("?sub_confirmation=1")) {
+      return `${channel_link}?sub_confirmation=1`;
     }
-    return apiData?.channelUrl;
+    return channel_link;
   };
 
   const handleSubscribe = () => {
-    const subscribeLink = getSubscribeLink(apiData?.channelUrl);
+    const subscribeLink = getSubscribeLink(apiData?.channel_link);
     let timer;
     window.open(subscribeLink, "_blank", "noopener,noreferrer");
 
@@ -37,14 +48,12 @@ const Show = (props)=> {
     return () => clearTimeout(timer)
   };
   const handleUnlockUrl = () => {
-    window.open(apiData?.unlockUrl, "_blank", "noopener,noreferrer");
+    window.open(unlock_link, "_blank", "noopener,noreferrer");
   };
-
-
 
   return (
     <div
-      className={`flex min-h-screen items-center  justify-center font-Pixel p-4}`}
+      className={`flex min-h-screen items-center justify-center font-Pixel p-4}`}
     >
       <div
         className="relative w-full max-w-2xl min-h-[400px] border-4 border-primary backdrop-blur-[10.4px] bg-[#0000007a] p-8 text-primary shadow-[8px_8px_0px_0px] shadow-primary/50 dark:shadow-primary/30"
@@ -52,27 +61,29 @@ const Show = (props)=> {
       >
 
         {!isSubscribed ? (
-          <div key={'id'}>
+          <div key={id}>
             <h1
               className="mb-16  text-2xl font-bold uppercase tracking-wider "
               style={{ textShadow: "2px 2px 0px #FF00FF" }}
             >
-              {apiData?.username}
+              {title}
             </h1>
+            
             <div className="absolute bottom-8 left-8 right-8">
-              <div className="flex justify-between items-center mb-8">
-                <p className=" text-sm text-start">
-                  Complete all the steps below to unlock the link button. When
-                  you have completed all the steps.
+              <p>{description}</p>
+              <div className="flex justify-between items-center my-[0.5rem]">
+                <p className="flex items-center text-sm text-start w-[28rem]">
+                Complete the actions and unlock the link <CircleHelp className="cursor-pointer mr-2 h-4" />
                 </p>
                 <Button
                   onClick={handleSubscribe}
-                  className="border-2 border-primary bg-background  text-primary hover:bg-primary hover:text-primary-foreground cursor-pointer"
+                  className="border-2 border-primary hover:border-transparent bg-background text-primary hover:bg-[#e62117] hover:text-primary cursor-pointer"
                 >
-                  <Youtube className="mr-2 h-4 w-4" />
-                  Subscribe Now
+                  <Youtube className="mr-2 h-4 w-sm" />
+                  Subscribe On Youtube
                 </Button>
               </div>
+            <div className="flex text-sm/6 text-primary/70"><Clock8 className="p-1 stroke-primary/70"/> {updated_at}</div>
             </div>
           </div>
         ) : (
